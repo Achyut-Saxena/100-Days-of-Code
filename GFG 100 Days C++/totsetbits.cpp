3c7ff30f6e9b0ca7a120df7f3dd54bd659707fae
Achyut-Saxena <69965983+Achyut-Saxena@users.noreply.github.com>
2021-09-23 20:49:38 +0530

    Create totsetbits.cpp

---
+++ GFG 100 Days C++/totsetbits.cpp
@@ -0,0 +1,65 @@
+/*
+You are given a number N. Find the total count of set bits for all numbers from 1 to N(both inclusive).
+
+Example 1:
+
+Input: N = 4
+Output: 5
+Explanation:
+For numbers from 1 to 4.
+For 1: 0 0 1 = 1 set bits
+For 2: 0 1 0 = 1 set bits
+For 3: 0 1 1 = 2 set bits
+For 4: 1 0 0 = 1 set bits
+Therefore, the total set bits is 5.
+*/
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        // Your logic here
+        int a = 0;
+        int x = 1;
+        while(x<=n)
+        {
+            x = x*2;
+            int m = (n+1)/x;
+            int r = (n+1)%x;
+            int tot = m*(x/2);
+            if(r>(x/2))
+                tot = tot + (r-(x/2));
+            a +=tot;
+        }
+        return a;
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  }
+	  return 0;
+}
+  // } Driver Code Ends
