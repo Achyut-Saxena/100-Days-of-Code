687afa04446eef063d2754677d0effe0d017c7c7
Achyut-Saxena <69965983+Achyut-Saxena@users.noreply.github.com>
2021-09-26 23:50:09 +0530

    Create largestsubarray.cpp

---
+++ GFG 100 Days C++/largestsubarray.cpp
@@ -0,0 +1,64 @@
+/*
+Given an array having both positive and negative integers. The task is to compute the length of the largest subarray with sum 0.
+
+Example 1:
+
+Input:
+N = 8
+A[] = {15,-2,2,-8,1,7,10,23}
+Output: 5
+*/
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        // Your code here
+        unordered_map<int,int>mp;
+       int i,sum=0,ans=0;
+       for(i=0;i<n;i++){
+           sum+=A[i];
+           if(sum==0)
+           ans=max(ans,i+1);
+           if(mp.find(sum)==mp.end())
+           mp[sum]=i;
+           else{
+               ans=max(ans,i-mp[sum]);
+           }
+       }
+       return ans;
+   }
+    
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+  // } Driver Code Ends
